9adec73e33d5416cdbec7d8f821c4093833d898e
Willian Marinho <willian.m.sousa@gmail.com>
2022-10-27 21:17:50 -0300

    get diretions implemented

--- scripts/game.js
+++ scripts/game.js
@@ -4,12 +4,13 @@ const gameBoard = document.getElementById('board')
 // atualiza a animação na tela 
 function gameLoop(currentTime) {
     window.requestAnimationFrame(gameLoop)
-    const secSinceLastRender = (currentTime - renderTime) / 1000
-    if (secSinceLastRender < 1 / snakeSpeed) return
+    const secSinceLastRender = (currentTime - renderTime) / 1000 //converte de miliseguntos para segundos
+    if (secSinceLastRender < 1 / snakeSpeed) return //tempo entre cada movimento em segundos
 
     renderTime = currentTime
 
     updateSnake()
+    updateFood()
     drawSnake()
 }
 
@@ -18,24 +19,27 @@ function gameLoop(currentTime) {
 
 //SNAKE PART
 
-const snakeSpeed = 1 
+const snakeSpeed = 5 
 const snakeBody = [{ x: 11, y: 11 }] 
 
+//MOVIMENTA A SNAKE PARA POSIÇÃO CORRETA
 function updateSnake () {
-    const inputDiretion = getDirection()
+    const direction = getDirection()  
     for(let i = snakeBody.length - 2; i >= 0; i--){
         snakeBody[i + 1] = { ...snakeBody[i] } // new object
     }
 
     snakeBody[0].x += direction.x
     snakeBody[0].y += direction.y
-}
 
+    updateFood()
+}
+//CONTROLA AS DIMENSÕES DA SNAKE
 function createSnake (gameBoard) {
-    snakeBody.forEach(segment => {
+    snakeBody.forEach(piece => {
         const snakeElement = document.createElement('div')
-        snakeElement.style.gridRowStart = segment.y
-        snakeElement.style.gridColumnStart = segment.x
+        snakeElement.style.gridRowStart = piece.y
+        snakeElement.style.gridColumnStart = piece.x
         snakeElement.classList.add('snake')
         gameBoard.appendChild(snakeElement)       
     })
@@ -44,12 +48,75 @@ function createSnake (gameBoard) {
 function drawSnake () {
     gameBoard.innerHTML = ''
     createSnake(gameBoard)
+    createFood(gameBoard)
 }
 
 //INPUT PART
-
 let direction = {x: 0, y: 0}
+let lastInputDirection = { x: 0, y: 0 }
+
+window.addEventListener('keydown' , e => {
+    switch (e.key) {
+        case 'ArrowUp':
+            if (lastInputDirection.y !== 0) break
+            direction = { x: 0, y: -1 }
+            break
+        case 'ArrowDown':
+            if (lastInputDirection.y !== 0) break
+            direction = { x: 0, y: 1 }
+            break
+        case 'ArrowLeft':
+            if (lastInputDirection.x !== 0) break
+            direction = { x: -1, y: 0 }
+            break
+        case 'ArrowRight':
+            if (lastInputDirection.x !== 0) break
+            direction = { x: 1, y: 0 }
+            break
+    }
+})
 
 function getDirection() {
-    return getDirection
+    lastInputDirection = direction
+    return direction
+}
+
+// FOOD PART
+let food = { x: 10, y: 1 }
+const expansionRate = 1
+
+function updateFood () {
+    if (onSnake(food)) {
+        expandSnake(expansionRate)
+        food = { x: 20, y: 10 }
+    }
+}
+//CONTROLA AS DIMENSÕES DO FOOD
+function createFood (gameBoard) {
+        const foodElement = document.createElement('div')
+        foodElement.style.gridRowStart = food.y
+        foodElement.style.gridColumnStart = food.x
+        foodElement.classList.add('food')
+        gameBoard.appendChild(foodElement)       
+    
+}
+
+function expandSnake(amount) {
+    newSegments += amount
+}
+
+function onSnake(position) {
+    return snakeBody.some(piece => {
+        return equalPositions(piece, position)
+    })
+}
+
+function equalPositions(pos1, pos2) {
+    return pos1.x === pos2.x && pos1.y === pos2.y
+}
+
+function addSegments() {
+    for (let i = 0; i < newSegments; i++) {
+        snakeBody.push({ ...snakeBody[snakeBody.length -1] }) //duplica o ultimo elemento do snake
+    }
 }
